a044490c97ab795b180d51e16ca31374113a7919
kevin-loehfelm <kevin.loehfelm@hashicorp.com>
2023-01-27 10:38:28 -0500

    Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,8 @@
 output "web1" {
   description = "Web 1 Instances and DNS Endpoint"
   value = {
+    "workspace"    = "${terraform.workspace}"
+    "label"        = var.label
     "dns_endpoint" = module.web1.dns_name
     "servers"      = module.web1.instances
     "size"         = module.web1.size
